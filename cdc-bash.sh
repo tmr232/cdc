b06b8d95576b3c1157a834e89baf24b83c050dc1
Tamir Bahar <tbahar@magicleap.com>
2016-09-15 17:36:10 +0300

    Added reverse cdc for bash.

--- cdc-bash.sh
+++ cdc-bash.sh
@@ -4,11 +4,23 @@ function cdc {
         dest=.
     fi
 
+    if [ $dest == ".." ]; then
+        cdc-up
+        return
+    fi
+
     cd $dest
 
     if [ $(ls -A1 | wc -l) -eq 1 ]; then
         if [ -d $(ls -A1 | head -1) ]; then
             cdc $(ls -A1 | head -1)
         fi
+    fi
+}
+
+function cdc-up {
+    cd ..
+    if [ $(ls -A1 | wc -l) -eq 1 ]; then
+        cdc-up
     fi
 }
