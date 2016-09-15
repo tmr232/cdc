fb81fcb5f7d077df1bb1659d57e65718c0364e1b
Tamir Bahar <tbahar@magicleap.com>
2016-09-15 17:32:56 +0300

    Added reverse direction for powershell.
    Powershell version of cdc now supports cdc ...

--- cdc-posh.ps1
+++ cdc-posh.ps1
@@ -6,9 +6,23 @@ The function digs into the directory tree to the first intersection.
 function cdc
 {
     Param([String]$dir)
+    
+    if ($dir -eq "..") {
+        cdc-up
+        return
+    }
+
     cd $dir
     $dirs = ls
     if ($dirs.Length -eq 1) {
         cdc $dirs[0]
+    }
+}
+
+function cdc-up
+{
+    cd ..
+    if ((ls).Length -eq 1) {
+        cdc-up
     }
 }
