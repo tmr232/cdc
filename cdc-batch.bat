0666c204cc5b80a7bf6cad4a2d6fad4b057fc706
Tamir Bahar <tbahar@magicleap.com>
2016-09-15 17:40:39 +0300

    Added reverse cdc support for batchfile.

--- cdc-batch.bat
+++ cdc-batch.bat
@@ -11,20 +11,39 @@ if %argC% LSS 1 (
     set dest=.
 )
 
+if %dest% EQU .. (
+    goto CdcUp  
+)
+
 :Entry
-cd %dest% 2>NUL
 
-if %errorlevel% neq 0 (
-    goto error
-)
+    cd %dest% 2>NUL
 
-:: Number of items in directory
-for /f %%i in (' "dir /B | findstr /R /N "^.*$" | find /C ":"" ') do set n=%%i
+    if %errorlevel% neq 0 (
+        goto Finish
+    )
 
-:: If only dir entry, cd into it
-if %n% == 1 (
-    for /f %%i in (' "dir /B" ') do set dest=%%i
-    goto entry
-)
+    :: Number of items in directory
+    for /f %%i in (' "dir /B | findstr /R /N "^.*$" | c:\windows\system32\find /C ":"" ') do set n=%%i
 
-:Error
+    :: If only dir entry, cd into it
+    if %n% == 1 (
+        for /f %%i in (' "dir /B" ') do set dest=%%i
+        goto entry
+    )
+
+goto Finish
+
+:CdcUp
+
+    cd ..
+
+    :: Number of items in directory
+    for /f %%i in (' "dir /B | findstr /R /N "^.*$" | c:\windows\system32\find /C ":"" ') do set n=%%i
+
+    :: If only dir entry, cd into it
+    if %n% == 1 (
+        goto CdcUp
+    )
+
+:Finish
